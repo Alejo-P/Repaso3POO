56e86f447a445798739537da128868f0cd81cd57
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-02 07:16:40 -0500

    Inicio

---
+++ src/DisenoN.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class DisenoN {
+    private JTabbedPane Pestanas;
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextPane textPane1;
+}
---
+++ src/principal.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class principal {
+    public static void main (String [] args)
+    {
+        JFrame vent = new JFrame();
+        vent.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    }
+}
